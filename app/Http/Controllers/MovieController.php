a2cf3a5e51c426a7bec7c371dd26d4e49abb8c28
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-21 16:24:44 +0100

    complete /add cards

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function index() {
-        return view('partials.movies');
+
+        $movies = Movie::all();
+        dump($movies);
+        return view('partials.movies', compact('movies'));
     }
 }
--- resources/views/partials/movies.blade.php
+++ resources/views/partials/movies.blade.php
@@ -2,6 +2,17 @@
 
 @section('main-content')
     <div class="container">
-        <h2>movies</h2>
+        <h2>Movies</h2>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-3">
+                    <div class="card mb-2">
+                        <h3>{{$movie->title}}</h3>
+                        <h4>{{$movie->date}}</h4>
+                        <h4>{{$movie->vote}}</h4>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
