4ea09b6a57edae763b4227937dc3567850050da1
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-21 15:11:32 +0100

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous'/>
+    <title>Laravel-controller-model</title>
+</head>
+<body>
+    <header>
+        @include('partials.header')
+    </header>
+
+    <main>
+        @yield('main-content')
+    </main>
+
+    <footer>
+        @include('partials.footer')
+    </footer>
+</body>
+</html>
